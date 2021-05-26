147e4b8ef2d1ca638ba0a96c034906bbe61a6ef5
rashed.apploye <rashedemran.apploye@gmail.com>
2021-05-26 10:39:52 +0600

    deploying to heroku

--- student_management_system_in_django/settings.py
+++ student_management_system_in_django/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "My Secret Key"# Consider using your secret key
+SECRET_KEY = "v0cx)yye(#^o#-tfor+^2*!2=6j&^kr0hdc$n36^lsq4*2afbk"# Consider using your secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['smswithdjango.herokuapp.com']
-ALLOWED_HOSTS = ['*']  # Not recommended but useful in dev mode
+ALLOWED_HOSTS = ['student-management-system-ehr.herokuapp.com','127.0.0.1']  # Not recommended but useful in dev mode
 
 
 # Application definition
